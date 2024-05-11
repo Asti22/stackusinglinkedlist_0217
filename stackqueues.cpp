a1c85085b4f2cdaabb6c9a42c5650bc004d1c4a6
Asti22 <uun3461@gmail.com>
2024-05-11 10:48:33 +0700

    membuat variable int main

--- stackqueues.cpp
+++ stackqueues.cpp
@@ -93,8 +93,21 @@ int main(){
            if (!stack.isEmpty()){
             stack.peek(); //get the value of the top element
            }
+           else {
+            cout <<"Stack is empty. No top value." <<endl;
+           }
+           break;
+
+        case 4: 
+          cout <<"Exiting program. "<<endl;
+          break;
+        default:
+          cout <<"Invalid choice. try again." <<;
+          break;
 
         }
+        cout <<endl;
 
     }
+    return 0;
 }
