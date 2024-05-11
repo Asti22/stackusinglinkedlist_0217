72ab470f4516ce920e0162c05dfbbe7af3ea424e
Asti22 <uun3461@gmail.com>
2024-05-11 10:02:34 +0700

    step 2 untuk menetapkan nilai

--- stackqueues.cpp
+++ stackqueues.cpp
@@ -24,5 +24,6 @@ public:
       //push operation : Insert an element onto the top of the stack
       int push(int value) {
           Node* newNode = new Node(); //1. Allocate memory for the new node
+          newNode->data = value; //2.assign value
       }
 };
